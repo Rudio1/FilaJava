807a5da5107bc672acfa82c1c747c4a7c0fa82a5
Rudio1 <guilhermerudio6@gmail.com>
2022-07-22 09:32:52 -0300

    CONCEITO FILA

---
+++ Fila.java
@@ -0,0 +1,58 @@
+//Aqui é aplicado o conceito de LISTA - First IN, First OUT
+
+class Fila {
+    int inicio;
+    int fim;
+    int tamanho; 
+    int qtdElementos;
+    int lista[];
+
+    public Fila() {
+        inicio= fim = -1;
+        tamanho = 200;
+        lista = new int[tamanho];
+        qtdElementos = 0;
+    }
+
+    public boolean estaVazia() {
+        if(this.qtdElementos == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean estaCheia(){
+        if(this.qtdElementos == this.tamanho - 1) {
+            return true;
+        } 
+        return false;
+    }
+
+    public void adicionar(int e) {
+        if( ! estaCheia()) {
+            if(this.inicio == -1) {
+                inicio = 0;
+            }
+            this.fim++;
+            this.lista[fim] = e;
+            this.qtdElementos++;
+        }
+    }
+
+    public void remover(){
+        if(!this.estaVazia()){
+            this.inicio++;
+            this.qtdElementos--;
+        }
+    }
+
+    public void imprimirLista() {
+        String elementos = "";
+        for(int i = inicio; i <= fim; i++) {
+            elementos+= lista[i] + " - ";
+            
+        }
+        
+        System.out.println(elementos);
+    }
+}
